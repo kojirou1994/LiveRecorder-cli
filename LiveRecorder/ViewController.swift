6a775cf73649cbf66d00d17d1636d4726e20a09f
Kojirou <Kojirouhtc@gmail.com>
2017-01-17 18:34:21 +0800

    Initial Commit

---
+++ LiveRecorder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LiveRecorder
+//
+//  Created by 王宇 on 2017/1/17.
+//  Copyright © 2017年 Putotyra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
